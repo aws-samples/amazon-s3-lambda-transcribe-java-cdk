6267ff2903acfde7ad762650dd0f78aa11b6eafd
Arun Thangavel <aruthan@amazon.com>
2023-06-29 22:28:59 -0700

    Added Copyright info on all file header and updated the LICENSE file

--- AudioTranscribeFunction/src/main/java/com/amazonaws/lambda/AudioTranscribe.java
+++ AudioTranscribeFunction/src/main/java/com/amazonaws/lambda/AudioTranscribe.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: MIT-0
+ */
+
 package com.amazonaws.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
